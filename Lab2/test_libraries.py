c6660e8876330a3996d4c798bb22a29304638d6a
AndroullaAndronikou <46893516+AndroullaAndronikou@users.noreply.github.com>
2019-01-30 15:56:15 +0000

    lab2

---
+++ Lab2/test_libraries.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 28 12:43:26 2019
+
+@author: antri
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sbs
+import tensorflow as tf
+import sklearn as sk
